51210e9906067262dacf0faa4c0b63949510ef3e
asg <sg.andre.dados1@gmail.com>
2023-03-30 21:48:56 -0300

    implement sentimenal analysis

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
 
 with open("FILE","r") as file:
   text = file.read()
@@ -16,9 +20,44 @@ for word in words:
     a[word] = a[word] + 1
   else:
     a[word] = 1
-print(a)
 
 alist = [(key,value) for (key,value) in a.items()]
 
-sorted(alist, key=lambda tup: tup [1],reverse=True)
+alist = sorted(alist, key=lambda tup: tup [1],reverse=True)
 
+
+english_stopwords = stopwords.words("english")
+
+listsstopwords = []
+
+for i in english_stopwords:
+  listsstopwords.append(i)
+
+
+filtred = []
+
+for i,n in alist:
+  if i not in listsstopwords:
+    filtred.append((i,n))
+
+
+analyser = SentimentIntensityAnalyzer()
+
+score = analyser.polarity_scores(text)
+
+patterns = re.compile("Chapter [0-9]+")
+chapters = re.split(patterns,text)
+
+chapters = chapters[1:]
+
+chapters = [i.replace("\n","") for i in chapters ]
+
+print(len(chapters))
+
+
+score = {}
+for index,chapter in enumerate(chapters):
+  score[index + 1] = analyser.polarity_scores(chapter)
+
+
+print(score)
